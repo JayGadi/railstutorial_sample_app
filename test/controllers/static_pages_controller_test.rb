054897c26f1c8e6bfea53eab23b54a8644cd8a23
jaygadi <jay.gadi0@gmail.com>
2016-02-04 01:49:53 -0500

    Chapter 5 - Layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_response :success
   end
 
+  test "should get contact" do
+    get :contact
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_response :success
+  end
+
 end
